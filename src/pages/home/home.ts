1e443dac09e9a803668f5493a7bb535d10dbf66b
patr9240 <patr9240@gmail.com>
2017-02-16 22:23:11 -0500

    Added ability to add an item, delete an item, and edit an item.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
@@ -9,20 +9,118 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 export class HomePage {
 
   listItems: FirebaseListObservable<any>;
-  
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire) {
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, 
+  af: AngularFire, public actionSheetCtrl: ActionSheetController) {
     this.listItems = af.database.list('/todoList');
   }
 
   //methods
-  addGame(){
-
+  addItem(){
+    let prompt = this.alertCtrl.create({
+    title: 'To-Do List Item',
+    message: "Add a new item to your To-Do List.",
+    inputs: [
+      {
+        name: 'item',
+        placeholder: 'Description'
+      },
+    ],
+    buttons: [
+      {
+        text: 'Save',
+        handler: data => {
+          this.listItems.push({
+            item: data.item,
+            done: 0
+          });
+        }
+      },
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.listItems.push({
+            item: data.item,
+            done: 0
+          });
+        }
+      }
+    ]
+  });
+  prompt.present();
+  }
+/*
+  showOptions(itemId, itemDescription) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Item Options?',
+      buttons: [
+        {
+          text: 'Delete Item',
+          role: 'destructive',
+          handler: () => {
+            this.removeItem(itemId);
+          }
+        },{
+          text: 'Update Item',
+          handler: () => {
+            this.updateItem(itemId, itemDescription);
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+*/
+  removeItem(itemId: string){
+    this.listItems.remove(itemId);
   }
 
-  editGame(){
+  updateItem(itemId, itemDescription){
+    let prompt = this.alertCtrl.create({
+      title: 'Item Description',
+      message: "Update the description for this To-Do List item.",
+      inputs: [
+        {
+          name: 'item',
+          placeholder: 'Description',
+          value: itemDescription
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.listItems.update(itemId, {
+              item: data.item
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+  editItem(){
 
   }
-  deleteGame(gameId) {
+  deleteItem() {
 
   }
 
